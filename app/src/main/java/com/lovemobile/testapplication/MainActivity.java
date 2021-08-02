a2c1f5753ca37fb513da27f3348f5ec01bb88db4
chanlung19951116 <48348242+lovemobile1234@users.noreply.github.com>
2021-08-01 20:37:36 -0400

    update gradlew

--- app/src/main/java/com/lovemobile/testapplication/MainActivity.java
+++ app/src/main/java/com/lovemobile/testapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String count = "1234";
+    String count = "12343";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
